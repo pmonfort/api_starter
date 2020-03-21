b83462f99e2e179d154674195b021cd50da05598
Pablo Monfort <pmonfort@gmail.com>
2020-03-21 19:40:56 -0300

    WIP - RailsApi iniatial specs generation

--- lib/generators/rails_api.rb
+++ lib/generators/rails_api.rb
@@ -17,6 +17,7 @@ module Generators
         create_migration(resource)
         create_model(resource, File.join(base_target_ps_path, 'app', 'models'))
         create_factories(resource)
+        create_specs(resource)
       end
 
       base_target_ps_path
@@ -38,6 +39,46 @@ module Generators
         resource,
         File.join(BASE_TEMPLATE_PATH, 'controller.erb'),
         File.join(base_target_ps_path, 'app', 'controllers', file_name)
+      )
+    end
+
+    # Specs
+    def create_specs(resource)
+      create_controller_spec(resource)
+      create_model_sepc(resource)
+      create_routing_spec(resource)
+    end
+
+    def create_controller_spec(resource)
+      file_name = "#{resource['plural_name']}_controller_spec.rb"
+      resource['name_downcase'] = resource['name'].downcase
+
+      create_file_from_template(
+        resource,
+        File.join(BASE_TEMPLATE_PATH, 'spec', 'controller.erb'),
+        File.join(base_target_ps_path, 'spec', 'controllers', file_name)
+      )
+    end
+
+    def create_model_sepc(resource)
+      file_name = "#{resource['name'].downcase}.rb"
+      resource['name_downcase'] = resource['name'].downcase
+
+      create_file_from_template(
+        resource,
+        File.join(BASE_TEMPLATE_PATH, 'spec', 'model.erb'),
+        File.join(base_target_ps_path, 'spec', 'models', file_name)
+      )
+    end
+
+    def create_routing_spec(resource)
+      file_name = "#{resource['plural_name']}_routing.rb"
+      resource['name_downcase'] = resource['name'].downcase
+
+      create_file_from_template(
+        resource,
+        File.join(BASE_TEMPLATE_PATH, 'spec', 'routing.erb'),
+        File.join(base_target_ps_path, 'spec', 'routing', file_name)
       )
     end
   end
